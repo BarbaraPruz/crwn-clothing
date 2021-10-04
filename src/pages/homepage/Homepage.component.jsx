b760556e375327b866810394ed9804f6eddc1104
Barbara <barbara.prusiewicz@gmail.com>
2021-10-04 10:23:55 -1000

    Refactor homepage into smaller components. Style menu items

---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import Directory from '../../components/directory/directory.component'
+import './Homepage.styles.scss'
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+}
+
+export default Homepage
