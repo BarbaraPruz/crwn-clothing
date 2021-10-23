4debf358ca7ca4fc66c026953fb3fa541e45b0f7
Barbara <barbara.prusiewicz@gmail.com>
2021-10-22 18:42:05 -1000

    Add sign up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/Shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 import './App.css'
 
@@ -22,9 +22,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscreFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({currentUser: user})
-      console.log('auth change',user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+          console.log('onsnapshot',this.state.currentUser)
+        } )
+      }
+      this.setState({currentUser: userAuth})
     })
   }
 
