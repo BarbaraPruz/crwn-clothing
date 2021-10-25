8e88c8e12c5ac067004717ac2fae347cd503f3f0
Barbara <barbara.prusiewicz@gmail.com>
2021-10-24 18:35:38 -1000

    Keep track of item quantity in cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,10 @@
+
+export const addItemToCart = (cartItems, itemToAdd) => {
+    const exists = cartItems.find( item => item.id === itemToAdd.id)
+    if (exists) {
+        return cartItems.map(cartItem =>
+            cartItem.id === itemToAdd.id ? {...cartItem, quantity: cartItem.quantity+1}
+            : cartItem)
+    }
+    return [...cartItems, {...itemToAdd, quantity: 1}]
+}
