594436cc22934b6c1e9d5ec6e4d6d75784e43e11
Barbara <barbara.prusiewicz@gmail.com>
2021-10-31 14:25:17 -1000

    Add increase/decrease quantity to checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,23 @@
-
 export const addItemToCart = (cartItems, itemToAdd) => {
-    const exists = cartItems.find( item => item.id === itemToAdd.id)
-    if (exists) {
-        return cartItems.map(cartItem =>
-            cartItem.id === itemToAdd.id ? {...cartItem, quantity: cartItem.quantity+1}
-            : cartItem)
-    }
-    return [...cartItems, {...itemToAdd, quantity: 1}]
+  const exists = cartItems.find((item) => item.id === itemToAdd.id)
+  if (exists) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === itemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem,
+    )
+  }
+  return [...cartItems, { ...itemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const existingItem = cartItems.find((item) => item.id === itemToRemove.id)
+  if (existingItem?.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== itemToRemove.id)
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === itemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem,
+  )
 }
